6b0672d6f16f837f6d1739a96c7d63042d79e971
David Grandinetti <dave@wegoto12.com>
2014-06-09 15:40:41 -0400

    added a bit of docs on how to change to a subcommand

--- lib/pod/command/COMMAND.rb
+++ lib/pod/command/COMMAND.rb
@@ -3,6 +3,11 @@ module Pod
     # This is an example of a cocoapods plugin adding a top-level subcommand
     # to the 'pod' command. Adapt it to suit your needs.
     #
+    # To make this a subcommand (e.g. `pod spec lint`):
+    #   - move this file down a directory
+    #   - this class should extend the command you are a subcommand of
+    #   - edit `lib/cocoapods_plugin.rb` to require to this file
+    #
     # @todo Create a PR to add your plugin to CocoaPods/cocoapods.org
     #       in the `plugins.json` file, once your plugin is released.
     #
