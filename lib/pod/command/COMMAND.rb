3e8078b0d96ee929017035ddc4ba943a2d18d331
David Grandinetti <dave@wegoto12.com>
2014-06-11 10:29:58 -0400

    modified the comments in the command template to give a better example of creating a subcommand for an existing or new command

--- lib/pod/command/COMMAND.rb
+++ lib/pod/command/COMMAND.rb
@@ -1,12 +1,18 @@
 module Pod
   class Command
     # This is an example of a cocoapods plugin adding a top-level subcommand
-    # to the 'pod' command. Adapt it to suit your needs.
+    # to the 'pod' command.
     #
-    # To make this a subcommand (e.g. `pod spec lint`):
-    #   - move this file down a directory
-    #   - this class should extend the command you are a subcommand of
-    #   - edit `lib/cocoapods_plugin.rb` to require to this file
+    # You can also create subcommands of existing or new commands. Say you
+    # wanted to add a subcommand to `list` to show newly deprecated pods,
+    # (e.g. `pod list deprecated`), there are a few things that would need
+    # to change.
+    #
+    # - move this file to `lib/pod/command/list/deprecated.rb` and update
+    #   the class to exist in the the Pod::Command::List namespace
+    # - change this class to extend from `List` instead of `Command`. This
+    #   tells the plugin system that it is a subcommand of `list`.
+    # - edit `lib/cocoapods_plugins.rb` to require this file
     #
     # @todo Create a PR to add your plugin to CocoaPods/cocoapods.org
     #       in the `plugins.json` file, once your plugin is released.
